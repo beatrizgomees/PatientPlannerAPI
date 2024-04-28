a26e73761c9910c3c7aabfde24570ca0cc7dbc5b
beatrizgomees <beatrizgomesxx@gmail.com>
2024-04-27 21:19:31 -0300

    adding openApi

--- src/main/java/com/github/beatrizgomees/api/rheumaPlanner/RheumaPlanner.java
+++ src/main/java/com/github/beatrizgomees/api/rheumaPlanner/RheumaPlanner.java
@@ -5,11 +5,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+
 public class RheumaPlanner {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
     }
