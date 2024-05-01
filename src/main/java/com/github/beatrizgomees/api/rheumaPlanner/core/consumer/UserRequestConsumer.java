636e3d5a95a5e39d5621c83639a83486c99281f3
beatrizgomees <beatrizgomesxx@gmail.com>
2024-05-01 14:49:19 -0300

    adding messaging with kafka ps:It is connected but the message is not arriving in the Quarkus application

---
+++ src/main/java/com/github/beatrizgomees/api/rheumaPlanner/core/consumer/UserRequestConsumer.java
@@ -0,0 +1,16 @@
+package com.github.beatrizgomees.api.rheumaPlanner.core.consumer;
+
+
+import jakarta.inject.Singleton;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+@Singleton
+public class UserRequestConsumer {
+
+
+    @Incoming("user-connect")
+    public void consumer(String message){
+        System.out.println("==== Message Recebida: " + message);
+    }
+}
