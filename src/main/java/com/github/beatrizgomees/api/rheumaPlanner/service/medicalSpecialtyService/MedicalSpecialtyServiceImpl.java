a0992d6fc162e9444022924ebab71a35661a4869
beatrizgomees <beatrizgomesxx@gmail.com>
2024-05-17 13:35:37 -0300

    remove package

--- src/main/java/com/github/beatrizgomees/api/rheumaPlanner/service/medicalSpecialtyService/MedicalSpecialtyServiceImpl.java
+++ src/main/java/com/github/beatrizgomees/api/rheumaPlanner/service/medicalSpecialtyService/MedicalSpecialtyServiceImpl.java
@@ -1,23 +1,10 @@
 package com.github.beatrizgomees.api.rheumaPlanner.service.medicalSpecialtyService;
 
-import com.github.beatrizgomees.api.rheumaPlanner.domain.doctor.Doctor;
-import com.github.beatrizgomees.api.rheumaPlanner.infrastructure.data.DataManager;
-import com.github.beatrizgomees.api.rheumaPlanner.domain.exceptions.FindByIdException;
-import com.github.beatrizgomees.api.rheumaPlanner.domain.exceptions.GetException;
-import com.github.beatrizgomees.api.rheumaPlanner.domain.CrudService;
 import com.github.beatrizgomees.api.rheumaPlanner.domain.medicalSpecialty.MedicalSpecialtyRequest;
-import com.github.beatrizgomees.api.rheumaPlanner.domain.medicalSpecialty.MedicalSpecialty;
-import com.github.beatrizgomees.api.rheumaPlanner.domain.medicalSpecialty.MedicalSpecialtyMapper;
 import com.github.beatrizgomees.api.rheumaPlanner.service.BaseCrudService;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCursor;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import org.bson.Document;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 @ApplicationScoped
 public class MedicalSpecialtyServiceImpl extends BaseCrudService<MedicalSpecialtyRequest, Document> {
 
